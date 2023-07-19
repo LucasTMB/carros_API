6a53a82261bb31de1d9c814ffe911feddeabd935
LucasTMB <lucas.tmb708@gmail.com>
2023-07-18 22:41:26 -0300

    Adicionando as funções de buscarUm, inserir, alterar e deletar. Com isso, o projeto está finalizado.

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,19 @@ const router = express.Router();
 
 const CarroController = require('./controllers/CarroController');
 
+// consultando todos os carros
 router.get('/carros', CarroController.buscarTodos);
+
+// consultando apenas um carro
+router.get('/carro/:codigo', CarroController.buscarUm);
+
+// inserindo carro no banco
+router.post('/carro', CarroController.inserir);
+
+// alterando dados
+router.put('/carro/:codigo', CarroController.alterar);
+
+// deletando um carro
+router.delete('/carro/:codigo', CarroController.deletar);
 
 module.exports = router;
